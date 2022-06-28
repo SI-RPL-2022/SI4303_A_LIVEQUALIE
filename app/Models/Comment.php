b327eae0df01019798d43589f90d862568b8a460
ftk2000 <wisanggeniario@gmail.com>
2022-06-28 13:59:03 +0700

    Add: [S1 - PBI20] Membuat comment forum resolve #8

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    
+    public function userc(){
+        return $this->belongsTo(User::class , 'user_id' , 'id');
+    }
 }
