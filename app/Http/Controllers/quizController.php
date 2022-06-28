67d68cc8bdfebe2d0598408ee4e4f3f4251a676d
DESKTOP-UO9VSHL\USER <ilaltkm11@gmail.com>
2022-06-28 14:16:15 +0700

    Add: [S3 - PBI14] Mengelola Quiz Admin resolve #19

--- app/Http/Controllers/quizController.php
+++ app/Http/Controllers/quizController.php
@@ -57,4 +57,58 @@ class QuizController extends Controller
             $salah2->save();
         }
     }
+    public function quizEdit($id , Request $req){
+        $data = $req->session()->all();
+        $question = Question::find($id);
+
+        return view('quizEdit' , ['data' => $data , 'question' => $question ]);
+    }
+
+    public function postquizEdit(Request $request , $id){
+        $question = Question::find($id);
+        $question->title = $request->title;
+        $question->update();
+
+        $cek = Answer::where('question_id' , $question->id);
+        $cek->delete();
+
+
+
+        $benar = new Answer();
+        $benar->question_id = $question->id;
+        $benar->choices = $request->benar;
+        $benar->is_correct = 1;
+        $benar->save();
+
+        $salah1 = new Answer();
+        $salah1->question_id = $question->id;
+        $salah1->choices = $request->salah1;
+        $salah1->is_correct = 0;
+        $salah1->save();
+
+
+        if ($request->salah2 != null){
+            $salah2 = new Answer();
+            $salah2->question_id = $question->id;
+            $salah2->choices = $request->salah2;
+            $salah2->is_correct = 0;
+            $salah2->save();
+
+
+        }
+
+        return redirect()->route('quiz.index');
+
+
+    }
+
+    public function quizDelete($id){
+        $question = Question::find($id);
+        $question->delete();
+
+        $cek = Answer::where('question_id' , $question->id);
+        $cek->delete();
+
+        return redirect()->route('quiz.index');
+    }
 }
